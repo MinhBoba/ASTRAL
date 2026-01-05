d5ce46363d68a9871a5733f18a2f8772afb72bda
K.Minh <trankhaiminh2706@gmail.com>
2026-01-05 10:37:38 +0700

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,14 +2,15 @@
 Utils package - Data loading, file handling, and constraint checking
 """
 
-from .data_loader import get_dataframe_from_table, InputData
-from .file_handler import save_solution_to_pickle, load_solution_from_pickle
+from .data_loader import get_dataframe_from_table, InputData, read_excel_sheet
+from .file_handler import save_metaheuristic_result, load_metaheuristic_result
 from .constraint_checker import find_violations
 
 __all__ = [
     'get_dataframe_from_table',
+    'read_excel_sheet', # Thêm cái này nếu bạn đã update data_loader
     'InputData',
-    'save_solution_to_pickle',
-    'load_solution_from_pickle',
+    'save_metaheuristic_result', # Tên đúng
+    'load_metaheuristic_result', # Tên đúng
     'find_violations',
 ]
