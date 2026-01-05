f8f53ef307988c7fbc3aca7fb340c1c4da3b779c
K.Minh <trankhaiminh2706@gmail.com>
2026-01-05 12:03:03 +0700

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,7 +6,8 @@ from .file_handler import (
     save_metaheuristic_result, 
     load_metaheuristic_result,
     save_model_solution,
-    load_model_solution
+    load_model_solution,
+    save_solution_to_pickle # Added the alias here too
 )
 from .constraint_checker import find_violations
 
@@ -17,5 +18,6 @@ __all__ = [
     'load_metaheuristic_result',
     'save_model_solution',
     'load_model_solution',
+    'save_solution_to_pickle',
     'find_violations',
 ]
